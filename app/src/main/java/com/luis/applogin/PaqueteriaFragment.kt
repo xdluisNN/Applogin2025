6b95c7ee48a94bc0b216e557e6503da967528bd2
xdluisNN <luismh2375@gmail.com>
2025-04-11 19:51:22 -0600

    mas cambios xd

--- app/src/main/java/com/luis/applogin/PaqueteriaFragment.kt
+++ app/src/main/java/com/luis/applogin/PaqueteriaFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [InicioFragment.newInstance] factory method to
+ * Use the [PaqueteriaFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class InicioFragment : Fragment() {
+class PaqueteriaFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,35 +34,8 @@ class InicioFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
+        return inflater.inflate(R.layout.fragment_paqueteria, container, false)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<LinearLayout>(R.id.btnPaqueteria).setOnClickListener {
-            // Navegar a Fragmento Paquetería
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, PaqueteriaFragment())
-                .addToBackStack(null)
-                .commit()
-        }
-
-        view.findViewById<LinearLayout>(R.id.btnAsociados).setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, AsociadosFragment())
-                .addToBackStack(null)
-                .commit()
-        }
-
-        view.findViewById<LinearLayout>(R.id.btnSistemas).setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, SistemasFragment())
-                .addToBackStack(null)
-                .commit()
-        }
-    }
-
 
     companion object {
         /**
@@ -72,12 +44,12 @@ class InicioFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment InicioFragment.
+         * @return A new instance of fragment PaqueteriaFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            InicioFragment().apply {
+            PaqueteriaFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
